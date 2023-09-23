f5e115eae4d0875edb90ee396e6b39acdbf4f1b9
arfath <arfath@linklet.io>
2023-09-23 20:00:12 +0530

    Adds Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,45 +1,166 @@
-import {
-  useAuthState,
-  useSignInWithEmailAndPassword,
-  useSignInWithGoogle,
-} from "react-firebase-hooks/auth";
-import { auth } from "../firebase/app.ts";
-import { useCallback, useState } from "react";
-import { redirect } from "react-router-dom";
+import {useState} from "react";
+import {useAuthState, useSignOut} from "react-firebase-hooks/auth";
+import {auth} from "../firebase/app.ts";
+import {useNavigate} from "react-router-dom";
+import {User} from "firebase/auth";
 
-export const Dashboard = () => {
-  const [emailLoginForm, setEmailLoginForm] = useState({
-    email: "",
-    password: "",
-  });
+// Profile Dropdown
+const ProfileDropDown = (props: { class: string; user: User }) => {
+  const [signOut] = useSignOut(auth);
 
-  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
-  const [signInWithGoogle] = useSignInWithGoogle(auth);
-
-  const [user] = useAuthState(auth, {
-    onUserChanged: async (user) => {
-      console.log(user);
-    },
-  });
-
-  const onSignInClick = useCallback(async () => {
-    await signInWithEmailAndPassword(
-      emailLoginForm.email,
-      emailLoginForm.password,
-    );
-  }, [signInWithEmailAndPassword, emailLoginForm]);
-
-  const onContinueWithGoogleClick = useCallback(async () => {
-    await signInWithGoogle();
-  }, [signInWithGoogle]);
-
-  if (user) {
-    redirect("/dashboard");
-  }
+  const [state, setState] = useState(false);
 
   return (
-    <main className="w-full h-screen flex flex-col items-center justify-center px-4">
-      Dashboard
-    </main>
+    <div className={`relative ${props.class}`}>
+      <div className="flex items-center space-x-4">
+        <button
+          className="w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 ring-2 lg:focus:ring-indigo-600"
+          onClick={() => setState(!state)}
+        >
+          <img
+            src={
+              props.user.photoURL ||
+              `https://ui-avatars.com/api/?name=${props.user.displayName}`
+            }
+            className="w-full h-full rounded-full"
+          />
+        </button>
+        <div className="lg:hidden">
+          <span className="block">{props.user.displayName}</span>
+          <span className="block text-sm text-gray-500">
+            {props.user.email}
+          </span>
+        </div>
+      </div>
+      <ul
+        className={`bg-white top-12 right-0 mt-5 space-y-5 lg:absolute lg:border lg:rounded-md lg:text-sm lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${
+          state ? "" : "lg:hidden"
+        }`}
+      >
+        <li>
+          <a
+            className="block text-gray-600 lg:hover:bg-gray-50 lg:p-2.5"
+            onClick={signOut}
+          >
+            Log out
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  if (!user) {
+    navigate("/login");
+  }
+
+  const [menuState, setMenuState] = useState(false);
+
+  if (!user) {
+    return <></>;
+  }
+
+  // Replace javascript:void(0) path with your path
+  const navigation = [
+    { title: "Customers", path: "javascript:void(0)" },
+    { title: "Careers", path: "javascript:void(0)" },
+    { title: "Guides", path: "javascript:void(0)" },
+    { title: "Partners", path: "javascript:void(0)" },
+  ];
+  return (
+    <nav className="bg-white border-b">
+      <div className="flex items-center space-x-8 py-3 px-4 max-w-screen-xl mx-auto md:px-8">
+        <div className="flex-none lg:flex-initial">
+          <a href="javascript:void(0)">
+            <img
+              src="https://www.floatui.com/logo.svg"
+              width={120}
+              height={50}
+              alt="Float UI logo"
+            />
+          </a>
+        </div>
+        <div className="flex-1 flex items-center justify-between">
+          <div
+            className={`bg-white absolute z-20 w-full top-16 left-0 p-4 border-b lg:static lg:block lg:border-none ${
+              menuState ? "" : "hidden"
+            }`}
+          >
+            <ul className="mt-12 space-y-5 lg:flex lg:space-x-6 lg:space-y-0 lg:mt-0">
+              {navigation.map((item, idx) => (
+                <li key={idx} className="text-gray-600 hover:text-gray-900">
+                  <a href={item.path}>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+            <ProfileDropDown class="mt-5 pt-5 border-t lg:hidden" user={user} />
+          </div>
+          <div className="flex-1 flex items-center justify-end space-x-2 sm:space-x-6">
+            <form className="flex items-center space-x-2 border rounded-md p-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 flex-none text-gray-300"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                className="w-full outline-none appearance-none placeholder-gray-500 text-gray-500 sm:w-auto"
+                type="text"
+                placeholder="Search"
+              />
+            </form>
+            <ProfileDropDown class="hidden lg:block" user={user} />
+            <button
+              className="outline-none text-gray-400 block lg:hidden"
+              onClick={() => setMenuState(!menuState)}
+            >
+              {menuState ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
