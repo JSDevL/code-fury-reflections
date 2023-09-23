c27dfe70f587a44db158f9f219f855ce4441d31f
arfath <arfath@linklet.io>
2023-09-23 19:12:45 +0530

    Adds firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "./pages/Login.tsx";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_AYpRsK2ZGvm4pryariA3vHGasePKNjk",
+  authDomain: "code-fury-reflections.firebaseapp.com",
+  projectId: "code-fury-reflections",
+  storageBucket: "code-fury-reflections.appspot.com",
+  messagingSenderId: "845240599343",
+  appId: "1:845240599343:web:b378231143881766c757da",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
